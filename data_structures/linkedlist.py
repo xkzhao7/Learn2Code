632a79569a719cd532fce60a15f9e7a1eddaafd3
xkzhao7 <xiaofeizhao7@gmail.com>
2025-06-27 18:36:35 +0800

    added reverse with pos input

--- data_structures/linkedlist.py
+++ data_structures/linkedlist.py
@@ -36,21 +36,47 @@ class LinkedList:
         for i in range(pos):
             n1 = n1.next
         n1.data = value
-    def reverse(self):
-        n1 = self.head
+    def reverse(self, pos1, pos2):
+        p1 = self.head
+        p2 = self.head
+        for i in range(pos1):
+            p1 = p1.next
+        for i in range(pos2):
+            p2 = p2.next
+        if p2 != self.tail:
+            n4 = self.head
+            for i in range (pos2 + 1):
+                n4 = n4.next
+        n1 = p1
         n2 = n1.next
         n3 = n2.next
-        while (n3 is not self.tail):
+        while n3 is not p2:
             n2.next = n1
             n1 = n2
             n2 = n3
             n3 = n2.next
         n2.next = n1
         n3.next = n2
-        n1 = self.head
-        self.head = n3
-        self.tail = n1
-        self.tail.next = None
+        if (p2 == self.tail) and (p1 == self.head):
+            self.tail = p1
+            p1.next = None
+            self.head = p2
+        elif (p2 == self.tail) and (p1 != self.head):
+            n1 = self.head
+            for i in range(pos1 - 1):
+                n1 = n1.next
+            n1.next = n3
+            self.tail = p1
+            p1.next = None
+        elif (p2 != self.tail) and (p1 == self.head):
+            p1.next = n4
+            self.head = p2
+        elif (p2 != self.tail) and (p1 != self.head):
+            n1 = self.head
+            for i in range(pos1 - 1):
+                n1 = n1.next
+            n1.next = p2
+            p1.next = n4
     def printList(self):
         n1 = self.head
         v = Vector()
@@ -70,8 +96,26 @@ if __name__ == "__main__":
     l.remove(9)
     l.printList()
 
-    l.reverse()
+    l.reverse(0,8)
     l.printList()
+    l.reverse(0,8)
+    l.printList()
+    print("")
 
-    l.reverse()
+    l.reverse(4,8)
     l.printList()
+    l.reverse(4,8)
+    l.printList()
+    print("")
+
+    l.reverse(0,4)
+    l.printList()
+    l.reverse(0,4)
+    l.printList()
+    print("")
+
+    l.reverse(2,6)
+    l.printList()
+    l.reverse(1,7)
+    l.printList()
+    print("")
