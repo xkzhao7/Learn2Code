8d88c0380a5e3ade7ce59563bd035d7238a6dc7e
xkzhao7 <xiaofeizhao7@gmail.com>
2025-07-27 12:19:56 -0700

    added has path function

--- data_structures/graph.py
+++ data_structures/graph.py
@@ -29,25 +29,32 @@ class Graph:
                 visited.add(current)
                 for neighbor in self.graph[current]:
                     queue.add(neighbor)
-
+    def has_path(self, node, destination, visited=None):
+        if visited is None:
+            visited = set()
+        if node not in visited:
+            if node == destination:
+                return True
+            visited.add(node)
+            for neighbor in self.graph[node]:
+                if self.has_path(neighbor, destination, visited):
+                    return True
+        return False
 if __name__ == "__main__":
     g = Graph()
 
-    g.add_node("A")
-    g.add_node("B")
-    g.add_node("C")
-    g.add_node("D")
-    g.add_node("E")
     g.add_node("F")
     g.add_node("G")
+    g.add_node("I")
+    g.add_node("H")
+    g.add_node("J")
+    g.add_node("K")
 
-    g.add_edge("A", "B")
-    g.add_edge("A", "C")
-    g.add_edge("B", "D")
-    g.add_edge("B", "E")
-    g.add_edge("C", "F")
-    g.add_edge("C", "G")
+    g.add_edge("F", "G")
+    g.add_edge("F", "I")
+    g.add_edge("I", "G")
+    g.add_edge("I", "K")
+    g.add_edge("G", "H")
+    g.add_edge("J", "I")
 
-    g.dfs("A")
-    print("")
-    g.bfs("A")
+    print(g.has_path("F", "J"))
